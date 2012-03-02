9f49c6ff3ce701a7b9c94677db77f9a48f7cc9c0
Gavin Morrice <gavin@gavinmorrice.com>
2012-03-02 15:38:00 +0000

    Pushing to Github

---
+++ some_ruby_file.rb
@@ -0,0 +1,19 @@
+# Public: An awesome class created just for you! xxx
+# 
+# 
+class MyClass
+
+  # When called, will hit the owner of the instance
+  # 
+  # banana - A tasty fruit, high in potassium
+  # 
+  # Examples:
+  #   hit_me(@banana) # => "Ouch! That really hurt!"
+  # 
+  # Returns a bitch of a right hook
+  def hit_me(banana)
+    # ... haduken!
+  end
+  
+end
+
